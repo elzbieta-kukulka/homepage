e70e62f97aa3102ed4c5e1e630681aeafc3425c6
Elżbieta Kukułka <elzbietakukulka4@gmail.com>
2020-07-12 14:21:22 +0200

    Add content to new HTML-element using JS

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ const name = "Ela";
 const age = 30;
 
 console.log(`Mam na imię ${name} i mam ${age} lat`);
+
+const homework = document.querySelector(".section--js");
+
+console.log(homework);
+
+homework.innerHTML = '<h2 class="section__title">Jestem zadaniem domowym</h2>';
